9c8e1755e06b6337966912a7a7a52cb1ec6eddb0
Luiz Ferraz <luiz@lferraz.com>
2023-06-18 16:47:25 -0300

    Move blog content

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -3,10 +3,9 @@ import { z } from "astro:content";
 export const blogSchema = z
   .object({
     author: z.string().optional(),
-    pubDatetime: z.date(),
+    pubDateTime: z.date(),
     title: z.string(),
     postSlug: z.string().optional(),
-    featured: z.boolean().optional(),
     draft: z.boolean().optional(),
     tags: z.array(z.string()).default(["others"]),
     ogImage: z.string().optional(),
