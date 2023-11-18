def5db6ba62541a9ba7912a5ad50a0b9ba21d043
Luiz Ferraz <luiz@lferraz.com>
2023-11-18 18:41:04 -0300

    Make middleware work on edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
-import 'astro';
 import {defineMiddleware} from 'astro:middleware';
-import {randomUUID} from 'node:crypto';
 
 export const onRequest = defineMiddleware((context, next) => {
   const {url, cookies, locals} = context;
 
-  locals.clientId = cookies.get('croct_client_id')?.value ?? randomUUID();
+  locals.clientId = cookies.get('croct_client_id')?.value ?? crypto.randomUUID();
   locals.croctPreview = cookies.get('croct_preview')?.value
     ?? url.searchParams.get('croct-preview')
     ?? undefined;
