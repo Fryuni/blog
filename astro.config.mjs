eb61d81c9b41e9931b3a6b56205c8aa9ea3642a1
Luiz Ferraz <luiz@lferraz.com>
2023-11-18 18:30:47 -0300

    Migrate to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,19 @@ import react from '@astrojs/react';
 import remarkToc from 'remark-toc';
 import remarkCollapse from 'remark-collapse';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 // noinspection ES6PreferShortImport -- This is not TS, so path aliases don't work
 import { SITE } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  output: 'static',
-  // adapter: netlify({
-  //   functionPerRoute: false,
-  // }),
+  output: 'hybrid',
+  adapter: vercel({
+    functionPerRoute: true,
+    imageService: true,
+    edgeMiddleware: false,
+  }),
 
   trailingSlash: 'never',
   integrations: [
