96dda2db7267f17b10dea9132f041c62700097dc
Luiz Ferraz <luiz@lferraz.com>
2023-12-15 18:34:43 -0300

    Config front-matter extension

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,21 @@
 import {docsSchema} from '@astrojs/starlight/schema';
+import {z} from 'astro/zod';
 import {defineCollection} from 'astro:content';
 
 export const collections = {
   docs: defineCollection({
-    schema: docsSchema(),
+    schema: docsSchema({
+      extend: z.object({
+        tags: z.array(z.string()).default([]),
+        madeByAi: z.object({
+          title: z.boolean().default(false),
+          tags: z.boolean().default(false),
+          tagline: z.boolean().default(false),
+        })
+          .strict()
+          .default({}),
+      }),
+    }),
   }),
   // i18n: defineCollection({type: 'data', schema: i18nSchema()}),
 };
