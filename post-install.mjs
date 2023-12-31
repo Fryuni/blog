e6b5d55a6cb8f21a785f637e2fb0ebf55f37eadc
Luiz Ferraz <luiz@lferraz.com>
2023-12-31 16:56:33 -0300

    --wip-- [skip ci]

--- post-install.mjs
+++ post-install.mjs
@@ -21,17 +21,39 @@ function runCmd(command) {
   });
 }
 
-async function vercelEnv() {
+function computeVendorEnv() {
+  const envKey = process.env.TURBO_CI_VENDOR_ENV_KEY;
+  if (envKey === undefined) {
+    return process.env;
+  }
+
+  const vendorEnv = {};
+
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith(envKey)) {
+      vendorEnv[key.slice(envKey.length)] = value;
+    }
+  }
+
+  return vendorEnv;
+}
+
+const vendor = computeVendorEnv();
+
+
+async function prepareVercel() {
   if (process.env.VERCEL !== '1') {
     // Do nothing when not deploying to Vercel
     return;
   }
 
-  console.log('process.env', inspect(process.env, { depth: null }));
+  console.log('process.env', inspect(vendor, { depth: null }));
 
-  await runCmd('git status');
-  await runCmd('git remote -v');
-  await runCmd('git branch -av');
+  const commitRef = vendor.GIT_COMMIT_REF;
+
+  await runCmd(`git remote add --no-tags -t ${commitRef} upstream https://gitlab.com/Fryuni/blog.git`);
+  await runCmd(`git fetch --unshallow upstream ${commitRef}`);
+  await runCmd(`git reset --soft upstream/${commitRef}`);
 
   throw new Error('This is a test error');
 }
