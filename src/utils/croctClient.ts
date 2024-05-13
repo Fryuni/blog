47986d0608feed0c8646654c2540155db8ffab82
Luiz Ferraz <luiz@lferraz.com>
2024-05-13 20:35:17 -0300

    --wip-- [skip ci]

--- src/utils/croctClient.ts
+++ src/utils/croctClient.ts
@@ -7,8 +7,32 @@ croct.plug({
     '/api/cid-assigner',
     window.location.href,
   ).toString(),
+
+  baseEndpointUrl: 'https://beta.api.croct.io',
 });
 
 window.croct = croct;
 
-export {croct};
+setTimeout(async () => {
+  const currentTag = Date();
+
+  await croct.user.edit().set('custom.tag', currentTag).save();
+
+  let counter = 0;
+
+  const interval = setInterval(async () => {
+    if (counter++ > 10) {
+      clearInterval(interval);
+    }
+
+    try {
+      const readTag = await croct.evaluate("user's tag", { timeout: 2000 });
+
+      console.log({ readTag, expectedTag: currentTag });
+    } catch (err) {
+      console.error(err);
+    }
+  }, 2000);
+}, 2000);
+
+export { croct };
