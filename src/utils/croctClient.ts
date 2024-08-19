4cee5e42c85a22ef051b2f57a6ce094b4047ad99
Luiz Ferraz <luiz@lferraz.com>
2024-08-19 12:37:32 -0300

    Use Croct's CID assigner

--- src/utils/croctClient.ts
+++ src/utils/croctClient.ts
@@ -3,7 +3,7 @@ import croct from '@croct/plug';
 croct.plug({
 	appId: import.meta.env.PUBLIC_CROCT_APP_ID,
 	// debug: import.meta.env.DEV,
-	cidAssignerEndpointUrl: new URL('/api/cid-assigner', window.location.href).toString(),
+	// cidAssignerEndpointUrl: new URL('/api/cid-assigner', window.location.href).toString(),
 
 	token: null,
 	// track: false,
